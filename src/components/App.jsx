f59ebae6155e99414a42f23077697103ab1ac885
Solod Sergey <Solod098@gmail.com>
2022-09-26 12:45:56 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import user from 'data/user.json';
+
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import { Box } from './Box/Box';
+import { Statisics } from './Statistics/Statistics';
+import { Profile } from './Profile/Profile';
+import { FriendsList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    <Box
+      bg="backGroundColor"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
     >
-      React homework template
-    </div>
+      <Profile user={user} />
+      <Statisics title="Upload stats" data={data} />
+      <FriendsList friends={friends}></FriendsList>
+      <TransactionHistory transactions={transactions}></TransactionHistory>
+    </Box>
   );
 };
